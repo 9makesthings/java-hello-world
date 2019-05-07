c22fcffc089aa679826eebfd9d0fb4037207ba20
Nina Johnson <9makesthings@gmail.com>
2019-05-07 15:33:39 -0500

    learning how to use javac

--- hello.java
+++ hello.java
@@ -1,7 +1,12 @@
+/**
+ * Hello
+ */
+
 public class Hello {
 
     public static void main(String[] args) {
         String x = "Hello World";
         System.out.println(x);
+        System.out.println("Woohoo I almost get Java C...");
     }
 }
