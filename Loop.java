55f6b41b7ca9bf01d0e335a899907fed9131ca36
Nina Johnson <9makesthings@gmail.com>
2019-05-07 16:49:58 -0500

    importing Loop class in Hello

---
+++ Loop.java
@@ -0,0 +1,9 @@
+
+public class Loop {
+
+    public static void loopArray(int[] array) {
+        for(int i=0; i < array.length; i++ ){
+            System.out.println("import test:" + array[i]);
+        }
+    }
+}
--- hello.java
+++ hello.java
@@ -2,11 +2,31 @@
  * Hello
  */
 
+import Loop;
+
 public class Hello {
 
     public static void main(String[] args) {
-        String x = "Hello World";
-        System.out.println(x);
+        int a = 20, b = 10, c = 0, d = 20, e = 40, f = 30; 
+        String x = "Thank", y = "You"; 
+        int[] numArray = {1, 2, 3, 4, 5};
+
+        System.out.println(x + ' ' + y);
         System.out.println("Woohoo I almost get Java C...");
+        System.out.println("a + b = " + (a + b)); 
+        System.out.println("1 + 2 = " + addNumbers(1, 2));
+
+        // Hello.loopNumbers(numArray);
+        Loop.loopArray(numArray);
+    }
+
+    public static int addNumbers(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void loopNumbers(int[] array) {
+        for(int i=0; i < array.length; i++ ){
+            System.out.println(array[i]);
+        }
     }
 }
