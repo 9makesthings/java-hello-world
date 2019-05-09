b2d669de570d0a4f7fc4cb25268e665861060a75
Nina Johnson <9makesthings@gmail.com>
2019-05-09 10:55:35 -0500

    updates

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,4 +1,4 @@
-package hello;
+package src.main.java.hello;
 
 public class HelloWorld {
   public static void main(String[] args) {
--- test/Hello.java
+++ test/Hello.java
@@ -8,12 +8,14 @@ public class Hello {
 
     public static void main(String[] args) {
         int a = 20, b = 10, c = 0, d = 20, e = 40, f = 30; 
-        String x = "Thank", y = "You"; 
+        String x = "Hello", y = "World"; 
         int[] numArray = {1, 2, 3, 4, 5};
 
         System.out.println(x + ' ' + y);
         System.out.println("Woohoo I almost get Java C...");
         System.out.println("a + b = " + (a + b)); 
+        System.out.println("c * d = " + (c * d)); 
+        System.out.println("e - f = " + (e - f)); 
         System.out.println("1 + 2 = " + addNumbers(1, 2));
 
         // Hello.loopNumbers(numArray);
